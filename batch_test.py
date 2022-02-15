7f692935ef5bcc07c719abb5214d6b62bc66c421
Nguyen Phuong Duy <duyn@princeton.edu>
2022-02-15 12:13:50 -0500

    fix bug of batch_test and add logger

--- batch_test.py
+++ batch_test.py
@@ -18,10 +18,20 @@ Max runtime before termination: 150
 
 import numpy as np
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(name)-20s '
+            '%(levelname)-8s %(message)s',
+    handlers=[
+        logging.FileHandler(
+            'batch_test.log'),
+        logging.StreamHandler()])
 
 # Constant params
 base_flag = "   python3 run.py                      \
-                --time_consistency                  \
                 --no_players 1                      \
                 --env_type goal_with_obs            \
                 --eps_control 0.1 --eps_state 0.1   \
@@ -29,16 +39,16 @@ base_flag = "   python3 run.py                      \
                 --linesearch_type trust_region      \
             "
 
-obstacle_flag = "--obstacles            \
-                    9.0, 25.0, 4.5      \
-                    20.0, 35.0, 3.0     \
-	                6.5, 50.0, 3.0      \
-                    1.0, 38.0, 3.0      \
-	                -1.0, 42.0, 2.0     \
-                    2.5, 50.0, 2.0      \
+obstacle_flag = " --obstacles            \
+                    9.0 25.0 4.5      \
+                    20.0 35.0 3.0     \
+	                6.5 50.0 3.0      \
+                    1.0 38.0 3.0      \
+	                -1.0 42.0 2.0     \
+                    2.5 50.0 2.0      \
                 "
 
-goal_flag = "--goal 6.0 40.0 2.0"
+goal_flag = " --goal 6.0 40.0 2.0"
 
 # Dynamic params
 no_of_runs = 100
@@ -48,12 +58,17 @@ theta = np.random.uniform(0, 2*np.pi, no_of_runs)
 v = np.random.uniform(3, 10, no_of_runs)
 time_horizon = np.random.randint(3, 11, no_of_runs)
 max_runtime = 150
+time_consistency = False
 
 for i in range(no_of_runs):
-    init_state_flag = "--init_states {} {} {} 0.0 {}".format(x[i], y[i], theta[i], v[i])
-    time_horizon_flag = "--time_horizon {}".format(time_horizon[i])
-    max_runtime_flag = "--max_steps {}".format(max_runtime)
+    init_state_flag = " --init_states {} {} {} 0.0 {}".format(x[i], y[i], theta[i], v[i])
+    time_horizon_flag = " --time_horizon {}".format(time_horizon[i])
+    max_runtime_flag = " --max_steps {}".format(max_runtime)
 
     cmd = base_flag + obstacle_flag + goal_flag + init_state_flag + time_horizon_flag + max_runtime_flag
+    if time_consistency:
+        cmd = cmd + " --time_consistency"
+    cmd = " ".join(cmd.split())
+    logging.info("Running test#{} with data: \n\r\t\n".format(i) + cmd.strip("\t"))
     os.system(cmd)
 
