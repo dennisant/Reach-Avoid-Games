b66ac577ffd5f93eed6b80604f222956cf86b482
Nguyen Phuong Duy <duyn@princeton.edu>
2022-02-28 11:56:28 -0500

    new run data

--- example/one_player_time_consistent.py
+++ example/one_player_time_consistent.py
@@ -13,9 +13,11 @@ sample_run_1 = "python3 run.py                                       \
                 --goal 6.0 40.0 2.0                                 \
                 --time_consistency                                  \
                 --alpha_scaling trust_region                        \
+                --trust_region_type ratio                           \
                 --t_horizon 12.0                                    \
                 --eps_control 0.1 --eps_state 0.1                   \
                 --hallucinated                                      \
+                --plot --log                                        \
                 ".format(np.pi/4.0)
 
 sample_run_2 = "python3 run.py                                      \
@@ -53,22 +55,4 @@ sample_run_3 = "python3 run.py                                      \
                 --hallucinated                                      \
                 ".format(np.pi/2.0)
 
-sample_run_4 = "python3 run.py                                      \
-                --env_type goal_with_obs                            \
-                --init_states 6.0 7.5 {} 0.0 8.0                    \
-                --no_players 1                                      \
-                --obstacles                                         \
-                        9.0 25.0 4.5                                \
-                        20.0 35.0 3.0                               \
-                        6.5 50.0 3.0                                \
-                        2.5 50.0 2.0                                \
-                --goal 6.0 40.0 2.0                                 \
-                --alpha_scaling trust_region                        \
-                --t_horizon 12.0                                     \
-                --eps_control 0.1 --eps_state 0.1                   \
-                --hallucinated                                      \
-                --time_consistency                                  \
-                --plot --log                                        \
-                ".format(np.pi/3.5)
-
-os.system(sample_run_4)
+os.system(sample_run_1)
--- example/one_player_time_inconsistent.py
+++ example/one_player_time_inconsistent.py
@@ -50,4 +50,22 @@ sample_run_3 = "python3 run.py                                      \
                 --plot --log                                        \
                 ".format(np.pi/3.5)
 
-os.system(sample_run_3)
+sample_run_4 = "python3 run.py                                       \
+                --env_type goal_with_obs                            \
+                --init_states 6.0 0.0 {} 0.0 8.0                    \
+                --no_players 1                                      \
+                --obstacles                                         \
+                        9.0 25.0 4.5                                \
+                        20.0 35.0 3.0                               \
+                        6.5 50.0 3.0                                \
+                        2.5 50.0 2.0                                \
+                --goal 6.0 40.0 2.0                                 \
+                --alpha_scaling trust_region                        \
+                --trust_region_type ratio                           \
+                --t_horizon 12.0                                    \
+                --eps_control 0.1 --eps_state 0.1                   \
+                --hallucinated                                      \
+                --plot --log                                        \
+                ".format(np.pi/4.0)
+
+os.system(sample_run_4)
